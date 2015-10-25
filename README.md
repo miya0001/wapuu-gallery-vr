2f3c7a2f907588a2a579d80b035f97db436b626e
miya0001 <miya@wpist.me>
2015-10-26 02:05:03 +0900

    get wapuu automatically

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ https://miya0001.github.io/wapuu-gallery-vr/
 
 ![](https://miya0001.github.io/wapuu-gallery-vr/img/screenshot.gif)
 
+## How to build
+
+```
+$ git clone git@github.com:miya0001/wapuu-gallery-vr.git
+$ cd wapuu-gallery-vr
+$ npm install
+$ bower
+$ gulp
+```
+
 ## Thanks
 
-https://github.com/borismus/webvr-boilerplate
+* https://github.com/jawordpressorg/wapuu
+* https://github.com/borismus/webvr-boilerplate
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var pngquant = require( 'imagemin-pngquant' );
+var filenamesToJson = require('gulp-filenames-to-json');
+var replace = require('gulp-replace');
+
+gulp.task( 'get_wapuus', function () {
+    return gulp.src( [
+            'node_modules/wapuu/wapuu-archive/*.png',
+            '!node_modules/wapuu/wapuu-archive/canvas-wapuu.png',
+            '!node_modules/wapuu/wapuu-archive/wapuu-france-hd.png',
+        ] )
+        .pipe( pngquant( { quality: '65-80', speed: 4 } )() )
+        .pipe( gulp.dest( 'wapuus' ) );
+});
+
+gulp.task( 'create_json', ['get_wapuus'], function () {
+    return gulp.src( [
+            'wapuus/*.png'
+        ] )
+        .pipe( filenamesToJson( { fileName: 'wapuu.json' } ) )
+        .pipe( replace( /^/, 'var wapuu = ' ) )
+        .pipe( replace( /$/, ';' ) )
+        .pipe( gulp.dest( '.' ) );
+});
+
+gulp.task( 'default', [ 'get_wapuus', 'create_json' ] );
