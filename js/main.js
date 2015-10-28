ebb645708203fef2d3c4212614c795e860439e4c
miya0001 <miya@wpist.me>
2015-10-28 14:36:05 +0900

    bug fix

--- js/main.js
+++ js/main.js
@@ -40,7 +40,6 @@ function rotate( timestamp ) {
 }
 
 function animate( timestamp ) {
-    renderer.render( scene, camera );
     controls.update();
     manager.render( scene, camera, timestamp );
     requestAnimationFrame( animate );
@@ -85,13 +84,6 @@ function add_logo( scene ) {
 }
 
 function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 11 );
-    controls = new THREE.VRControls( camera );
-    effect = new THREE.VREffect( renderer );
-    effect.setSize( window.innerWidth, window.innerHeight );
-    manager = new WebVRManager( renderer, effect, { hideButton: false } );
-
     add_cosmos( scene );
     add_logo( scene );
 
@@ -146,4 +138,11 @@ renderer.setPixelRatio( window.devicePixelRatio );
 document.body.appendChild( renderer.domElement );
 
 var scene, camera, controls, effect, manager, logo, wapuu_objects;
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 11 );
+controls = new THREE.VRControls( camera );
+effect = new THREE.VREffect( renderer );
+effect.setSize( window.innerWidth, window.innerHeight );
+manager = new WebVRManager( renderer, effect, { hideButton: false } );
+
 init();
